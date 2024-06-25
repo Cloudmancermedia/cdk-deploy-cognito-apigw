78acd661a229ecef9adeeda1992b2c2236655dc2
Ryan Williams <ryan.j.williams@charter.com>
2024-06-25 12:16:45 -0600

    chore: first commit for user pool

---
+++ lib/cdk-deploy-cognito-apigw-stack.ts
@@ -0,0 +1,85 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+export class CdkDeployCognitoApigwStack extends cdk.Stack {
+
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Define the Cognito User Pool
+    const userPool = new cognito.UserPool(this, 'MyUserPool', {
+      userPoolName: 'my-user-pool',
+      selfSignUpEnabled: true,
+      signInAliases: { email: true, phone: true },
+      autoVerify: { email: true, phone: true },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+    });
+
+    // Output the User Pool ID
+    new cdk.CfnOutput(this, 'UserPoolId', {
+      value: userPool.userPoolId,
+    });
+
+    // Define the Cognito User Pool Client
+    const userPoolClient = new cognito.UserPoolClient(this, 'MyUserPoolClient', {
+      userPool,
+      generateSecret: false,
+    });
+
+    // Output the User Pool Client ID
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: userPoolClient.userPoolClientId,
+    });
+
+    // Define the Lambda function
+    const myFunction = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log('request:', JSON.stringify(event, undefined, 2));
+          return {
+            statusCode: 200,
+            headers: { 'Content-Type': 'text/plain' },
+            body: \`Hello, \${event.requestContext.authorizer.claims['cognito:username']}!\`,
+          };
+        };
+      `),
+    });
+
+    // Define the API Gateway
+    const api = new apigateway.RestApi(this, 'MyApi', {
+      restApiName: 'My Service',
+      description: 'This service serves as an example.',
+    });
+
+    // Create a Cognito authorizer
+    const authorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'MyAuthorizer', {
+      cognitoUserPools: [userPool],
+    });
+
+    // Create an endpoint
+    const lambdaIntegration = new apigateway.LambdaIntegration(myFunction);
+    const resource = api.root.addResource('myendpoint');
+    resource.addMethod('GET', lambdaIntegration, {
+      authorizer,
+      authorizationType: apigateway.AuthorizationType.COGNITO,
+    });
+
+    // Output the API Gateway URL
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
+    });
+  }
+}
